60c3bac3d88d3424a03835076e1d06da7d2d939f
arikashehzad2 <arikashehzad2@gmail.com>
2023-11-06 16:58:29 +0500

    updated

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
         TaxiManager<Taxi> manager = new TaxiManager<>();
 
-        Taxi taxi1 = new Taxi(1, false, new int[]{9, 2});
-        Taxi taxi2 = new Taxi(2, true, new int[]{8, 9});
-        Taxi taxi3 = new Taxi(3, true, new int[]{1, 5 });
+        Taxi taxi1 = new Taxi(1, false, new int[]{8, 2});
+        Taxi taxi2 = new Taxi(2, true, new int[]{6, 65});
+        Taxi taxi3 = new Taxi(3, true, new int[]{1, 9 });
 
         manager.addTaxi(taxi1);
         manager.addTaxi(taxi2);
